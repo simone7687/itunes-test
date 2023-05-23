5a0876547d4a25b8d06a1ccd26d14a82ae9e41b9
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2023-05-23 21:44:18 +0200

    getTopSongs

--- src/BaseRestService.ts
+++ src/BaseRestService.ts
@@ -1,11 +1,14 @@
 import axios from 'axios';
+import { Song } from 'model/Song';
 
-async function getUser(artistName: string) {
+async function getTopSongs(artistName: string, limit: number = 50): Promise<Song[]> {
     try {
         artistName = artistName.replace(' ', '+').toLocaleLowerCase();
-        const response = await axios.get(`https://itunes.apple.com/search?term=${artistName}&entità=canzone&limit=50`);
+        const response = await axios.get<Song[]>(`https://itunes.apple.com/search?term=${artistName}&entità=canzone&limit=${limit}`);
         console.log(response);
+        return response.data;
     } catch (error) {
         console.error(error);
+        return [];
     }
 }
